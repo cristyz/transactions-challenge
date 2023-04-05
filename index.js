62ffd0ab13b99b9e0e2361b3c2f1d46a0a7dc16e
crist <crist.soares2013@live.com>
2023-04-05 13:29:58 -0300

    feat: groupBy without lodash

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import groupBy from 'lodash.groupby';
 import { transactions } from './transactions.js';
+
+const groupBy = (array, key) => {
+    return array.reduce((acc, item) => {
+        const group = item[key];
+
+        if (!acc[group]) acc[group] = [];
+
+        acc[group].push(item);
+        return acc;
+    }, {});
+}
 
 console.log(groupBy(transactions, 'date'));
